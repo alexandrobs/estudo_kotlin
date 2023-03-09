d83bbcda7848992cf49403fc724bdf58687b2ccf
alexandrobs <jornalsinal@gmail.com>
2023-03-08 21:59:33 -0300

    Exercicios 4 de kotlin resolvido

--- src/main/kotlin/teste_kotlin_fundamental/Exercicio04Funcoes.kt
+++ src/main/kotlin/teste_kotlin_fundamental/Exercicio04Funcoes.kt
@@ -3,9 +3,10 @@ package teste_kotlin_fundamental
 //4. Escreva uma função capaz de receber milhas e converter em km(1 milha = 1,6km)
 
 fun main(){
-
+    converteMilhaEmKm(10)
 }
 
 fun converteMilhaEmKm(milha: Int){
-    println(milha)
+    var resultado = milha * 1.6
+    println("$milha kilometros = $resultado milhas")
 }
