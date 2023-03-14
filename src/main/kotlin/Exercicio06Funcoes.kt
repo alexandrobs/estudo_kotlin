cba8f5d1a59ee94c07d7377c7ed68ca0b3834997
alexandrobs <jornalsinal@gmail.com>
2023-03-13 23:14:11 -0300

    Exercicios 5 e 6 corrigido e outros ajustes

--- src/main/kotlin/Exercicio06Funcoes.kt
+++ src/main/kotlin/Exercicio06Funcoes.kt
@@ -1,25 +1,31 @@
 
-//funcao 02 transformada em funcao de uma unica linha
+/* 6. Sobre as funções criadas nos exercícios 2, 3 e 4. É possível transformá-las em funções de uma única linha? Se sim,
+transforme-as.
+ */
 
 fun main(){
 
     contaCaracterString("Alexandro")
     calculaCuboInteiro(10)
     converteMilhaEmKm(1)
+
     trocaLetras("america")
     trocaLetras2("america")
     trocaLetras3("america")
+
     println(teste_kotlin_fundamental.trocaLetras("america"))
 
 }
 
+// Ex. 02
 fun contaCaracterString(texto: String) = println(texto.length)
-
+// Ex. 03
 fun calculaCuboInteiro(number: Int) = println(number*number*number)
-
+// Ex. 04
 fun converteMilhaEmKm(milha: Int) = println(milha * 1.6)
+// Ex. 05
 fun trocaLetras(letra: String) = println(letra.lowercase().replace("a","x"))
-
+// Ex. 05
 fun trocaLetras2(letra: String) = println(letra.replace("a","x",true).lowercase())
-
+// Ex. 05
 fun trocaLetras3(letra: String) = println(letra.replace("A","x").replace("a", "x").lowercase())
--- src/main/kotlin/teste_kotlin_fundamental/Exercicio05Funcoes.kt
+++ src/main/kotlin/teste_kotlin_fundamental/Exercicio05Funcoes.kt
@@ -1,5 +1,11 @@
 package teste_kotlin_fundamental
 
+/* Escreva um programa que seja capaz de receber uma string e fazer a troca de todas as letras "a" ou "A" por "x".
+a. Não deve existir lógica dentro da função main. Deve ser usada somente como ponto de entrada.
+b. Escrever uma função para a troca de letras e impressão do valor final.
+c. String final deve estar com todas as letras minúsculas.
+ */
+
 fun main(){
 
     println(trocaLetras("america"))
@@ -9,5 +15,5 @@ fun main(){
 fun trocaLetras(letra: String): String {
     var novaLetra = letra.replace("a","x")
     novaLetra.replace("A","x")
-    return novaLetra.uppercase()
+    return novaLetra.lowercase()
 }
