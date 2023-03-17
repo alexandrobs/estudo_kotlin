338f5b61ea97b535e5761ed1d8ebad7645587241
alexandrobs <jornalsinal@gmail.com>
2023-03-16 22:23:25 -0300

    exercicios refatorado de if-else e teste com when em substituição ao ifelse para calculo de bonus

--- src/main/kotlin/teste_kotlin_fundamental/estudos_diversos_por_conteudo/WhenOperator.kt
+++ src/main/kotlin/teste_kotlin_fundamental/estudos_diversos_por_conteudo/WhenOperator.kt
@@ -1,7 +1,20 @@
 package teste_kotlin_fundamental
 
-//when operator
+//when - função calcula bonus
+fun bonusWhen(cargo: String): Float {
+    return when (cargo) {
+        "Gerente" -> 2000f
+        "Coordenador" -> 1500f
+        "Engenheiro de Software" -> 1000f
+        "Estagiário" -> 500f
+        else -> 0f
+    }
+}
 
 fun main(){
-
+    println(bonusWhen("Gerente"))
+    println(bonusWhen("aasasasddad"))
+    println(bonusWhen("Coordenador"))
+    println(bonusWhen("Engenheiro de Software"))
+    println(bonusWhen("Estagiário"))
 }
--- src/main/kotlin/teste_kotlin_fundamental/exercicios_if_else/ExercicioIfElse.kt
+++ src/main/kotlin/teste_kotlin_fundamental/exercicios_if_else/ExercicioIfElse.kt
@@ -9,33 +9,55 @@ c. Engenheiros de software recebem R$ 1.000,00
 d. Estagiários recebem R$ 500.00
 */
 
+fun calculaBonus(cargo: String): Float {
+    if( cargo == "Gerente" ) {
+        return 2000f
+    }
+    if ( cargo == "Coordenador" ) {
+        return 1500f
+    }
+    if ( cargo == "Engenheiro de Software" ) {
+        return 1000f
+    }
+    if ( cargo == "Estagiário" ) {
+        return 500f
+    } else {
+        return 0f
+    }
+}
+
+data class Empregado (
+    var cargo: String,
+    var bonusFinalAno: Float)
+
+val gerente = Empregado("Gerente", 0f)
+val coordenador = Empregado("Coordenador", 0f)
+val engenheiroSoftware = Empregado("Engenheiro de Software", 0f)
+val estagiario = Empregado("Estagiário", 0f)
+
 fun main(){
 
-    data class Empregado (
-        var cargo: String,
-        var bonusFinalAno: Double)
-
-    val gerente = Empregado("Gerente", 0.00)
-    val coordenador = Empregado("Coordenador", 0.00)
-    val engenheiroSoftware = Empregado("Engenheiro de Software", 0.00)
-    val estagiario = Empregado("Estagiário", 0.00)
-
     if(  gerente.cargo == "Gerente" ) {
-        gerente.bonusFinalAno = 2.000
+        gerente.bonusFinalAno = 2000f
     }
     if (  coordenador.cargo == "Coordenador" ) {
-        coordenador.bonusFinalAno = 1.500
+        coordenador.bonusFinalAno = 1500f
     }
     if ( engenheiroSoftware.cargo == "Engenheiro de Software" ) {
-        engenheiroSoftware.bonusFinalAno = 1.000
+        engenheiroSoftware.bonusFinalAno = 1000f
     }
     if ( estagiario.cargo == "Estagiário" ) {
-        estagiario.bonusFinalAno = 500.0
+        estagiario.bonusFinalAno = 500f
     }
 
     println(gerente.bonusFinalAno)
     println(coordenador.bonusFinalAno)
     println(engenheiroSoftware.bonusFinalAno)
     println(estagiario.bonusFinalAno)
+
+    println(calculaBonus("Gerente"))
+    println(calculaBonus("Coordenador"))
+    println(calculaBonus("Engenheiro de Software"))
+    println(calculaBonus("Estagiário"))
 
 }
