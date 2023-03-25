86573159d9198d528d11b137d7fcb3da25c9826c
alexandrobs <jornalsinal@gmail.com>
2023-03-24 23:48:50 -0300

    exercicios de for e while 1, 2 e 3 resolvidos

---
+++ src/main/kotlin/teste_kotlin_fundamental/exercicios_for_while/ExerciciosForWhile.kt
@@ -0,0 +1,58 @@
+package teste_kotlin_fundamental.exercicios_for_while
+
+/*
+
+CONTROLE DE FLUXO - FOR
+
+1. Imprima os números de 1 a 50 na mesma linha em ordem crescente. Saída esperada:
+"1 2 3 4 5 6 7 8 9 10...49 50"
+
+2. Imprima os números de 1 a 50 na mesma linha em ordem descrescente. Saída esperada:
+"50 49 48 47... 2 1"
+
+3. Usando a resolução do exercício 1, não imprima os números múltiplos de 5.
+
+4. Faça a soma de todos os números no intervalo de 1 a 500.
+
+5. Escreva um programa capaz de receber um número inteiro N e imprimir uma escada de tamanho N usando o caracter #.
+
+    Se a entrada do programa for 3, teremos:                    Se a entrada do programa for 5, teremos:
+
+    #                                                           #
+    ##                                                          ##
+    ###                                                         ###
+                                                                ####
+                                                                #####
+
+CONTROLE DE FLUXO - WHILE
+
+
+ */
+
+fun main() {
+
+    //ex 1
+    for (i in 1..50) {
+        print("$i ")
+    }
+
+    println()
+
+    //ex 2
+    for (x in 50 downTo 1) {
+        print("$x ")
+    }
+
+    println()
+
+    //ex 3
+    for (z in 1..50) {
+        if (z % 5 == 0 || z % 5 == 5) {
+            print(" ")
+        } else {
+            print("$z ")
+        }
+    }
+
+    println()
+}
