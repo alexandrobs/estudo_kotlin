588e5c49f5fbc9e4ec1c1d9a199ea9101d8912c3
alexandrobs <jornalsinal@gmail.com>
2023-03-31 22:58:57 -0300

    exercicios de while 1 resolvido

--- src/main/kotlin/teste_kotlin_fundamental/exercicios_for_while/ExerciciosWhile.kt
+++ src/main/kotlin/teste_kotlin_fundamental/exercicios_for_while/ExerciciosWhile.kt
@@ -3,8 +3,9 @@ package teste_kotlin_fundamental.exercicios_for_while
 /*
 CONTROLE DE FLUXO - WHILE
 
-1. Considere uma caixa d'água de 2 mil litros. Rômulo gostaria de encher a caixa d'água de 7 litros. Quantos balões
-cabem na caixa d'água sem que o volume seja excedido?
+1. Considere uma caixa d'água de 2 mil litros.
+Rômulo gostaria de encher a caixa d'água com balões de 7 litros.
+Quantos balões cabem na caixa d'água sem que o volume seja excedido?
 
 2.FizzBuss. Imprima os números de 1 a 50 em ordem crescente de acordo com a regra abaixo:
     a. Quando um número for divisível por 3, imprimir Buzz.
@@ -19,4 +20,26 @@ true ou false. Caso não exista nem 'x' nem 'o', retornar false.
     "xxooox" -> true    "xxxxo" -> false    "bdefghjij" -> false    "ooooxzzzzz" -> false
 
 */
+fun qtdBaloesEncherPiscina() {
+    var baloes = 7
+    var cxDagua = 2000
+    var contador = 0
+    var qtdBaloes = 0
+
+    while(qtdBaloes < cxDagua) {
+        contador++
+        qtdBaloes = baloes * contador
+        if (qtdBaloes == cxDagua || qtdBaloes > cxDagua) {
+            contador--
+            var litrosTotal = contador * baloes
+            println("Encheu a piscina com $contador baloes de 7 litros, resultando em $litrosTotal litros")
+            break
+        }
+    }
+}
+
+fun main(){
+    qtdBaloesEncherPiscina()
+}
+
 
