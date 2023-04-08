d15071e5a8ba8110e8600c04fb913ade3cc59a69
alexandrobs <jornalsinal@gmail.com>
2023-04-07 21:52:18 -0300

    testando NullPointerException, valores nulos e algumas exceptions, try e catch, herança das exceptions

--- src/main/kotlin/teste_kotlin_fundamental/null_safety/TestaNullSafety.java
+++ src/main/kotlin/teste_kotlin_fundamental/null_safety/TestaNullSafety.java
@@ -2,6 +2,7 @@ package teste_kotlin_fundamental.null_safety;
 
 public class TestaNullSafety {
     public static void main(String[] args) {
+        //exemplo de NullPointerException
         String s = null;
         System.out.println(s.length());
     }
--- src/main/kotlin/teste_kotlin_fundamental/null_safety/TestaNullSafetyKt.kt
+++ src/main/kotlin/teste_kotlin_fundamental/null_safety/TestaNullSafetyKt.kt
@@ -1,9 +1,34 @@
 package teste_kotlin_fundamental.null_safety
 
 fun main() {
+    //exemplo de NullSafety
     var s: String? = null
+    println(s)
 
-    if (s != null) {
-        println(s.length)
+    //buscando outra exception
+
+    //println(num1)
+    //pega a exception e coloca num catch
+    try {
+        //var num1 = 10/0
+        //println(num1)
+        println(s!!.length)
+    } catch (e: java.lang.ArithmeticException) {
+        println("Nao se pode dividir por 0 $e")
+    } catch (e: java.lang.NullPointerException) {
+        println("variavel nula $e")
+        //esse finally e opcional
+    } catch (e: java.lang.Exception) {
+        println("Generica")
+    } finally {
+        println("Finally")
     }
+
+    try {
+        //assumindo que não é nulo
+        println(s!!.length)
+    } catch (e: NullPointerException) {
+        println("variavel nula $e")
+    }
+
 }
