5a6675677e983473e9c2fd4219619f5e440de1fc
alexandrobs <jornalsinal@gmail.com>
2023-04-18 23:14:33 -0300

    testando mais metodos com muitos parametros, com valores padrao, com parametro vararg e generics

---
+++ src/main/kotlin/teste_kotlin_fundamental/sobre_parametros/MaisParametros.kt
@@ -0,0 +1,50 @@
+package teste_kotlin_fundamental.sobre_parametros
+
+fun main() {
+
+    println(media(7f, 6f, 4f))
+    println(media(10f, 9f,8f, 5f))
+    println(media(10f, 8f))
+    println(media(10f, 8f, 9f, 5f, 6f, 9f))
+    println(media(false, 10f, 8f, 2, "", false, 98f))
+
+    //ex de uso real de vararg
+    arrayOf(2, 2, 3, 6, 2, 6, 45f, false)
+
+    //ex de uso de valor padrao nos parametros
+    "skalslasl".startsWith("sk", false)
+
+}
+
+//ex funcao com 3 parametros
+fun media(n1: Float, n2: Float, n3: Float): Float {
+    val soma = n1 + n2 + n3
+    return soma / 3;
+}
+
+//ex funcao com 4 parametros
+fun media(n1: Float, n2: Float, n3: Float, n4: Float): Float {
+    val soma = n1 + n2 + n3 + n4
+    return soma / 4;
+}
+
+//como criar uma funcao que receba n parametros?
+fun media(vararg notas: Float): Float {
+    var soma = 0f
+    for (n in notas) {
+        soma += n
+    }
+    return (soma / notas.size)
+}
+
+//usando generics, se tiver mais de um parametro Generic e usar vararg use ele sempre no final dos parametros
+fun <T, J> media(abc: J, vararg notas: T): Float {
+    var soma = 0f
+    for (n in notas) {
+        if (n is Float) {
+            soma += n
+        }
+    }
+    return (soma / notas.size)
+}
+
