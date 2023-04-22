98ea335d7bed4f18aa96ca5744408e7db537d53b
alexandrobs <jornalsinal@gmail.com>
2023-04-21 23:10:04 -0300

    collections, iterando com for corrigido

--- src/main/kotlin/teste_kotlin_fundamental/collections/TestaCollections.kt
+++ src/main/kotlin/teste_kotlin_fundamental/collections/TestaCollections.kt
@@ -2,11 +2,16 @@ package teste_kotlin_fundamental.collections
 
 fun main() {
 
+    //iterando uma lista com for
+    for (i in lista) {
+        println(i)
+    }
+
+    //or
+    for (i in lista.indices) {
+        println("item at $i is ${lista[i]}")
+    }
 }
 
 val lista = listOf("mamao", "maca", "banana")
 
-//iterando uma lista
-for (i in lista) {
-    println(i)
-}
