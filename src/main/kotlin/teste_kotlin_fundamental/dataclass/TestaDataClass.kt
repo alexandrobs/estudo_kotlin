b3b8bf406e6dd8ca1539254786cecd91b036c5ec
alexandrobs <jornalsinal@gmail.com>
2023-05-02 22:49:38 -0300

    class vs data class

--- src/main/kotlin/teste_kotlin_fundamental/dataclass/TestaDataClass.kt
+++ src/main/kotlin/teste_kotlin_fundamental/dataclass/TestaDataClass.kt
@@ -19,12 +19,19 @@ fun main() {
     println("data class")
     val f3: FormaData = FormaData(10, 8)
     val f4: FormaData = FormaData(10, 8)
+    var f5: FormaData
     println(f3.equals(f4))
     println(f3.hashCode())
     println(f3.toString())
     println("-------------")
     println(f4.hashCode())
     println(f4.toString())
+
+    println("************")
+
+    //copy e outro metodo que ja vem implementado no data class, class n tem
+    f5 = f3.copy(50)
+    println(f5.toString())
 
 }
 
@@ -50,3 +57,7 @@ data class FormaData(val a: Int, val b: Int)
 //data class ja implementa equals comparando valores e n enderecos de memoria como na class
 //na class temos que implementar esses metodos, equals, toString e hashCode
 //hashcode ja vem trazendo baseado nos valores dos atributos, diferente do class
+
+//outra utilidade para data class, facilitar passando ela no metodo ao inves de passar um monte de parametros
+data class Endereco(val rua: String, val cep: String, val city: String)
+fun endereco(e: Endereco) {}
