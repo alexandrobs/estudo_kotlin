b679cc90e2a420456b738192dfb41f85d1aabf43
alexandrobs <jornalsinal@gmail.com>
2023-05-03 23:10:29 -0300

    usando modificadores de acesso, public, protected e private

--- src/main/kotlin/teste_kotlin_fundamental/heranca/TestaHeranca.kt
+++ src/main/kotlin/teste_kotlin_fundamental/heranca/TestaHeranca.kt
@@ -1,15 +1,32 @@
 package teste_kotlin_fundamental.heranca
 
+//modificadores: public(n precisa colocar e o default), private, protected
+
+//uma class privada pode ser instanciada no mesmo arquivo, em outro arquivo ela n podeprivate class x
+//ela tb impede que a classe seja herdada
+private class x
 //class tem que ser open para outra classe poder herdar ela
 open class Eletronico(var marca: String) {
-    fun ligar() {}
+
+    private var s: String = ""
+    //modificador private
+    private fun ativarCorrente() {}
+
+    //protected da visibilidade a class e quem herda ela
+    protected fun ativarCorrente2() {}
+    fun ligar() {
+        ativarCorrente()
+    }
     fun desligar() {}
 }
 
 //a variavel n pode ser var pq a que ela herda ja tem isso
 class Computador(marca: String) : Eletronico(marca) {
     fun instalarSoftware() {}
-    fun processar() {}
+    fun processar() {
+        //como essa class herda Eletronico posso chamar seu metodo protected
+        ativarCorrente2()
+    }
 }
 
 fun main() {
@@ -17,4 +34,13 @@ fun main() {
     c.ligar()
     c.desligar()
     println(c.marca)
+    //ativarCorrente so pode ser usado por um eletronico
+    //c.ativarCorrente()
+    //nao e possivel acessar uma var private tb por outra classe
+    //c.s
+    //instanciando uma class privada
+    x()
+    //aqui eu n consigo herdar o metodo protected pq n estou no escopo da class nem de quem herda ela
+    //c.ativarCorrente2()
+
 }
---
+++ src/main/kotlin/teste_kotlin_fundamental/modificadores_visibilidade/TestaModificadoresVisibilidade.kt
@@ -0,0 +1,5 @@
+package teste_kotlin_fundamental.modificadores_visibilidade
+
+fun main() {
+
+}
