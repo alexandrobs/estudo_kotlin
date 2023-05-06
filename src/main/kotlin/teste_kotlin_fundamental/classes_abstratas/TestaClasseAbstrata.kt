e0649a3a6408804e0d763cc1a2b079e88c99d7e5
alexandrobs <jornalsinal@gmail.com>
2023-05-06 00:04:15 -0300

    testando interface

--- src/main/kotlin/teste_kotlin_fundamental/classes_abstratas/TestaClasseAbstrata.kt
+++ src/main/kotlin/teste_kotlin_fundamental/classes_abstratas/TestaClasseAbstrata.kt
@@ -3,10 +3,23 @@ package teste_kotlin_fundamental.classes_abstratas
 //herança n pode ser multipla em kotlin, mas podemos usar interface
 
 interface Selvagem {
-    fun atacar()
+    fun atacar() {
+        print("atacar")
+    }
+    //interface n guarda estado
+    //var m = ""
+}
+
+interface Voador {
+    fun voar() {
+        print("voar")
+    }
 }
 
 abstract class Mamifero(val nome: String) {
+
+    //classe abstrata guarda
+    var m = ""
     fun acordar() {
         println("acordar")
     }
@@ -17,7 +30,7 @@ abstract class Mamifero(val nome: String) {
     abstract fun falar()
 }
 
-class Cachorro(nome: String) : Mamifero(nome) {
+class Cachorro(nome: String) : Mamifero(nome), Selvagem, Voador {
     override fun falar() {
         TODO("Not yet implemented")
     }
@@ -27,4 +40,11 @@ class Cachorro(nome: String) : Mamifero(nome) {
 fun main(){
     //classe abstrata n pode ser implementada
     //val m = Mamifero()
+
+    val cao = Cachorro("Doguinho")
+    cao.dormir()
+    cao.acordar()
+    cao.voar()
+    cao.atacar()
+
 }
