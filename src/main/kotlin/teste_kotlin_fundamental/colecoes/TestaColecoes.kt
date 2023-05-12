62549e1eeaa26af691e4fa451edd9fa72221ee5c
alexandrobs <jornalsinal@gmail.com>
2023-05-11 23:39:09 -0300

    testando Set

--- src/main/kotlin/teste_kotlin_fundamental/colecoes/TestaColecoes.kt
+++ src/main/kotlin/teste_kotlin_fundamental/colecoes/TestaColecoes.kt
@@ -1,7 +1,10 @@
 package teste_kotlin_fundamental.colecoes
 
+//List, Set e Map
+
 fun main() {
 
+    val list0 = listOf(1, 2, 3, 4, 5)
     val list1: List<Int> = listOf(1, 2, 3, 4, 5)
     val list2: MutableList<Int> = mutableListOf(1, 2, 3, 4, 5)
 
@@ -17,5 +20,31 @@ fun main() {
     list2.clear()
 
     println(list2)
+
+    listOf<Int>(1, 32, 23, 454)
+
+    val set1 = setOf<Int>(1, 2, 3, 4, 5)
+    val set2 = mutableSetOf<Int>(1, 2, 3, 4, 5)
+
+    //ou listas vazias
+    listOf<Int>()
+    val set3: Set<String> = setOf<String>("ladrao", "bandido", "pilantra", "otario", "otario")
+    val set4: MutableSet<String> = mutableSetOf<String>()
+
+    println(set3)
+
+    println(set4)
+    set4.add("Boston")
+    set4.add("Sao Paulo")
+    println(set4)
+    set4.remove("Sao Paulo")
+    println(set4)
+    println(set4.contains("Sao Paulo"))
+    println(set4)
+    set4.clear()
+    println(set4)
+    println(set4.size)
+    set4.clear()
+    println(set4)
 
 }
