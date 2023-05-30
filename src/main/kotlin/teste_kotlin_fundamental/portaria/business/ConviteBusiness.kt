a44462cb3a8d0b6159231cb38d2e74fc766bf90e
alexandrobs <jornalsinal@gmail.com>
2023-05-29 23:05:21 -0300

    projeto portaria - refatoração da função codigoValido

--- src/main/kotlin/teste_kotlin_fundamental/portaria/business/ConviteBusiness.kt
+++ src/main/kotlin/teste_kotlin_fundamental/portaria/business/ConviteBusiness.kt
@@ -1,11 +1,19 @@
 package teste_kotlin_fundamental.portaria.business
 
+import teste_kotlin_fundamental.portaria.entity.Convite
+
 class ConviteBusiness {
     fun tipoValido(tipo: String): Boolean = tipo == "comum" || tipo == "premium" || tipo == "luxo"
 
-    fun codigoValido(codigo: String, tipo: String): Boolean {
-        return if (tipo == "comum" && codigo.startsWith("xt")) {
-            true
-        } else (tipo == "premium" || tipo == "luxo") && codigo.startsWith("xl")
+//    fun codigoValido(codigo: String, tipo: String): Boolean {
+//        return if (tipo == "comum" && codigo.startsWith("xt")) {
+//            true
+//        } else (tipo == "premium" || tipo == "luxo") && codigo.startsWith("xl")
+//    }
+
+    fun codigoValido(convite: Convite) = when (convite.tipo) {
+        "comum" -> convite.codigo.startsWith("xt")
+        "premium", "luxo" -> convite.codigo.startsWith("xl")
+        else -> false
     }
 }
